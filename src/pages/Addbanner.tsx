42e04a2ae992e4871375cf556ce526c3fe752b17
ridwanullah <wetwan080@gmail.com>
2025-07-06 09:45:04 +0100

    banner is fechted , added banner is comfir with cloudnary,

--- src/pages/Addbanner.tsx
+++ src/pages/Addbanner.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { Label } from "@/components/ui/label";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../config/Firebase";
+import { useNavigate } from "react-router";
 
 const Addbanner = () => {
-  return <div>Addbanner</div>;
+  const [banner, setBanner] = useState<File | null>(null);
+  const [name, setName] = useState<string>("");
+  const [uploading, setUploading] = useState(false);
+  const navigate = useNavigate();
+  const uploadImageToCloudinary = async (file: File): Promise<string> => {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", "martin");
+
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/dlu80k3sn/image/upload",
+        formData
+      );
+      return response.data.secure_url;
+    } catch (error) {
+      toast.error("Cloudinary upload failed");
+      throw new Error("Image upload failed");
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!banner) {
+      toast.error("Please select a banner image first.");
+      return;
+    }
+
+    try {
+      setUploading(true);
+      const imageUrl = await uploadImageToCloudinary(banner);
+
+      await addDoc(collection(db, "slider"), {
+        name: name,
+        imageUrl: imageUrl,
+      });
+      navigate("/banner");
+      toast.success("Banner uploaded successfully!");
+    } catch (error) {
+      console.error("Upload failed:", error);
+      alert("Upload failed. Please try again.");
+    } finally {
+      setUploading(false);
+    }
+  };
+
+  return (
+    <div className="w-3/4 mx-auto shadow shadow-green-400 p-5 rounded-md mt-10">
+      <h2 className="my-5 ml-7 font-extrabold">Add banner </h2>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-5 justify-center items-center"
+      >
+        <div className="grid w-full max-w-sm items-center gap-3">
+          <Label htmlFor="baner">Banner Name</Label>
+          <Input
+            type="text"
+            id="baner"
+            placeholder="banner name"
+            className="focus-visible:border-2 focus-visible:border-green-400 focus-visible:ring-[0px] capitalize "
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-3">
+          <Label htmlFor="firstname">Banner</Label>
+          <Input
+            type="file"
+            className="focus-visible:border-2 focus-visible:border-green-400 focus-visible:ring-[0px] capitalize "
+            onChange={(e) => setBanner(e.target.files?.[0] || null)}
+          />
+        </div>
+
+        <Button disabled={uploading}> submit</Button>
+      </form>
+    </div>
+  );
 };
 
 export default Addbanner;
--- src/pages/Banners.tsx
+++ src/pages/Banners.tsx
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { Button } from "@/components/ui/button";
 import { db } from "../../config/Firebase";
-import { collection, getDocs, query } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, query } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import { Link } from "react-router";
+import { ArchiveX, ChevronRightIcon, Delete, DeleteIcon } from "lucide-react";
+import { toast } from "react-toastify";
 
 interface banner {
   id: string;
@@ -27,18 +31,56 @@ const Banners = () => {
     setIsLoading(false);
   };
 
-  console.log(slider);
+  const Delete = async (id: string) => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this banner?"
+    );
+    if (!confirmed) return;
+
+    try {
+      const productDoc = doc(db, "slider", id);
+      await deleteDoc(productDoc);
+      toast.success("Banner deleted successfully");
+    } catch (error) {
+      console.error("Error deleting banner:", error);
+      toast.error("Failed to delete banner");
+    }
+  };
   useEffect(() => {
     getSlider();
   }, []);
 
   return (
-    <div className="flex ">
-      {slider.map((item) => (
-        <div key={item.id}>
-          <img src={item.imageUrl} width={100} height={100} />
-        </div>
-      ))}
+    <div>
+      <div className="flex justify-between items-center my-5">
+        <h1 className="capitalize font-bold text-xl">banners</h1>
+        <Button variant="outline" asChild>
+          <Link to="/addbanner">Add Banner</Link>
+        </Button>
+      </div>
+      <div className="flex items-center gap-2 ">
+        {slider.map((item) => (
+          <div
+            key={item.id}
+            className="w-[200px] h-[200px] rounded-2xl relative overflow-hidden"
+          >
+            <img
+              src={item.imageUrl}
+              width={100}
+              height={100}
+              className="w-full h-full overflow-hidden object-center"
+            />
+            <Button
+              variant="secondary"
+              size="icon"
+              className="size-8 absolute bottom-2 right-2 bg-red-600"
+              onClick={() => Delete(item.id)}
+            >
+              <ArchiveX className="text-white" />
+            </Button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
